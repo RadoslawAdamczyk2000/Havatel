78a5785bdf382f322357aa625ee73c1b95718a2b
Radosław Adamczyk <greatpolandweb@gmail.com>
2022-03-03 14:53:03 +0100

    generate atom components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavItemComponent } from './components/atoms/nav-item/nav-item.component';
+import { UserItemComponent } from './components/atoms/user-item/user-item.component';
+import { TodoItemComponent } from './components/atoms/todo-item/todo-item.component';
+import { PostItemComponent } from './components/atoms/post-item/post-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavItemComponent,
+    UserItemComponent,
+    TodoItemComponent,
+    PostItemComponent
   ],
   imports: [
     BrowserModule,
