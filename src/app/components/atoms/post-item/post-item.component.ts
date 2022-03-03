d28ab0e381d81d46ca902d6fc9e376cdd3797393
Radosław Adamczyk <greatpolandweb@gmail.com>
2022-03-03 16:51:52 +0100

    create post snap card

--- src/app/components/atoms/post-item/post-item.component.ts
+++ src/app/components/atoms/post-item/post-item.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-item.component.scss']
 })
 export class PostItemComponent implements OnInit {
-
+  content='Cernuus barba qui. Canonicus iure voluptates. Bis tibi quam. Molestiae enim solutio. Soluta aiunt amplexus. Laborum qui cupio. Assentator eaque asc...'
+  name='Radosław Adamczyk'
+  title='Templum sufficio creber dapifer in cursus vir'
   constructor() { }
 
   ngOnInit(): void {
